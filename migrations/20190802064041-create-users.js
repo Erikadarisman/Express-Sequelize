76bfb4377d70b7020e1ad804565ea2b15f8c7ada
kdrsm <eryx466@gmail.com>
2019-08-02 23:15:32 +0700

    first

---
+++ migrations/20190802064041-create-users.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER(6)
+      },
+      name: {
+        type: Sequelize.STRING(50)
+      },
+      email: {
+        type: Sequelize.STRING(50)
+      },
+      gender: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
